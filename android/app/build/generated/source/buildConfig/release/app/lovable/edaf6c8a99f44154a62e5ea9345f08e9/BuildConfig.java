9a52508dd42c490f00edc2eb3fd7a4c6be947afd
Your Name <neerajmadkar35@gmail.com>
2025-06-26 18:19:28 +0530

    Initial commit with Capacitor Android project

--- android/app/build/generated/source/buildConfig/release/app/lovable/edaf6c8a99f44154a62e5ea9345f08e9/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/app/lovable/edaf6c8a99f44154a62e5ea9345f08e9/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "app.lovable.edaf6c8a99f44154a62e5ea9345f08e9";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 3;
-  public static final String VERSION_NAME = "1.0.2";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.0.3";
 }
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,10 +5,6 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.edaf6c8a99f44154a62e5ea9345f08e9',
   appName: 'aavishkar-career-compass',
   webDir: 'dist',
-  server: {
-    url: 'https://edaf6c8a-99f4-4154-a62e-5ea9345f08e9.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   android: {
     buildOptions: {
       keystorePath: undefined,
